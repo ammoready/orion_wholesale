ab8eed575382c31ad6fdd9ec52a56da63dcf6bf0
Tony Beninate <tonybeninate@icloud.com>
2020-02-21 11:42:06 -0500

    Wire up Inventory. ammoready/ammoready-two-point-oh#693

--- lib/orion/inventory.rb
+++ lib/orion/inventory.rb
@@ -1,8 +1,8 @@
 module Orion
   class Inventory < Base
-
-    INVENTORY_FILENAME = 'exportXML_cq_barcodeFFL.csv'.freeze
-    FULL_INVENTORY_FILENAME = 'exportXML_cq_barcodeFFL_all.csv'.freeze
+    
+    CATALOG_DIR             = '/ammoready'.freeze
+    CATALOG_FILENAME_PREFIX = 'orion_inv_web'.freeze
 
     def initialize(options = {})
       requires!(options, :username, :password)
@@ -20,37 +20,53 @@ module Orion
     end
 
     def all
-      items    = []
-      tempfile = get_file(INVENTORY_FILENAME)
+      tempfile = get_most_recent_file(CATALOG_FILENAME_PREFIX, CATALOG_DIR)
+      items = []
 
-      CSV.foreach(tempfile, { headers: :first_row }).each do |row|
+      File.open(tempfile).each_with_index do |row, i|
+        row = row.split("\t")
+        
+        if i==0
+          @headers = row
+          next
+        end
+
         item = {
-          item_identifier: row['id'],
-          quantity:        row['qty'].to_i,
-          price:           row['cost'],
+          item_identifier: row[@headers.index('Item ID')].strip,
+          quantity:        row[@headers.index('Qty available')].to_i,
+          price:           row[@headers.index('Price')].strip,
         }
 
         items << item
       end
 
+      tempfile.close
       tempfile.unlink
 
       items
     end
 
     def quantity
-      items    = []
-      tempfile = get_file(FULL_INVENTORY_FILENAME)
+      tempfile = get_most_recent_file(CATALOG_FILENAME_PREFIX, CATALOG_DIR)
+      items = []
 
-      CSV.foreach(tempfile, { headers: :first_row }).each do |row|
+      File.open(tempfile).each_with_index do |row, i|
+        row = row.split("\t")
+        
+        if i==0
+          @headers = row
+          next
+        end
+
         item = {
-          item_identifier: row['id'],
-          quantity:        row['qty'].to_i,
+          item_identifier: row[@headers.index('Item ID')].strip,
+          quantity:        row[@headers.index('Qty available')].to_i,
         }
 
         items << item
       end
 
+      tempfile.close
       tempfile.unlink
 
       items
