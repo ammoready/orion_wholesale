d990189ad3fbb565cdf71f42c3262d17445a08c6
Tony Beninate <tonybeninate@icloud.com>
2020-09-18 11:05:55 -0400

    Account authentication ammoready/ammoready-two-point-oh#693

--- lib/orion_wholesale/user.rb
+++ lib/orion_wholesale/user.rb
@@ -1,14 +1,24 @@
 module OrionWholesale
   class User < Base
 
+    USER_DIR             = '/ammoready'
+    USER_FILENAME_PREFIX = 'orion_ardy_cust_tab'
+
     def initialize(options = {})
-      requires!(options, :username, :password)
+      requires!(options, :username, :password, :account_id)
       @options = options
     end
 
     def authenticated?
-      connect(@options) { |ftp| ftp.pwd }
-      true
+      tempfile = get_most_recent_file(USER_FILENAME_PREFIX, USER_DIR)
+
+      File.open(tempfile).each_with_index do |row, i|
+        row = row.split("\t")
+
+        return true if row[0].strip.downcase == @options[:account_id].strip.downcase
+      end
+
+      false
     rescue OrionWholesale::NotAuthenticated
       false
     end
